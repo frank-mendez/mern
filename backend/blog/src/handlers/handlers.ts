b8436952a6cbc59a8e4c0a72ba6dc558db5c6d0b
frankmendez <frankmendezresources@gmail.com>
2023-07-19 12:51:38 +0800

    Creating User Signup Request

--- backend/blog/src/handlers/handlers.ts
+++ backend/blog/src/handlers/handlers.ts
@@ -1,8 +1,9 @@
-import { GraphQLList, GraphQLObjectType, GraphQLSchema } from 'graphql'
+import { GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
 import { BlogType, CommentType, UserType } from '../schema/schema'
 import User from '../models/User'
 import Blog from '../models/Blog'
 import Comment from '../models/Comment'
+import { hashSync } from 'bcryptjs'
 
 const RootQuery = new GraphQLObjectType({
 	name: 'RootQuery',
@@ -23,6 +24,31 @@ const RootQuery = new GraphQLObjectType({
 			type: GraphQLList(CommentType),
 			async resolve() {
 				return await Comment.find()
+			},
+		},
+	},
+})
+
+const mutation = new GraphQLObjectType({
+	name: 'mutation',
+	fields: {
+		signup: {
+			type: UserType,
+			args: {
+				name: { type: GraphQLNonNull(GraphQLString) },
+				email: { type: GraphQLNonNull(GraphQLString) },
+				password: { type: GraphQLNonNull(GraphQLString) },
+			},
+			async resolve(parent, { email, name, password }) {
+				try {
+					const existingUser = User.findOne({ email })
+					if (existingUser) return new Error('User already exists')
+					const encrpytedPassword = hashSync(password)
+					const user = new User({ name, email, password: encrpytedPassword })
+					return await user.save()
+				} catch (error) {
+					return new Error('User signup failed. Try again')
+				}
 			},
 		},
 	},
